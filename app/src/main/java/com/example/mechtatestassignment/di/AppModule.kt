e95925115c2961bcf13e723740750d5ca4b01e54
Bekzat <altyn.bekzat01@gmail.com>
2024-05-19 16:04:46 +0500

    AppModule updated, application name added

--- app/src/main/java/com/example/mechtatestassignment/di/AppModule.kt
+++ app/src/main/java/com/example/mechtatestassignment/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.example.mechtatestassignment.di
 
+import com.example.mechtatestassignment.data.remote.RemoteRepositoryImpl
 import com.example.mechtatestassignment.data.remote.api.MechtaApi
+import com.example.mechtatestassignment.domain.repository.RemoteRepository
+import com.example.mechtatestassignment.domain.useCase.GetSmartphonesUseCase
+import kotlinx.coroutines.Dispatchers
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.coroutines.CoroutineContext
 
 
 val appModule = module {
@@ -14,4 +20,12 @@ val appModule = module {
             .build()
             .create(MechtaApi::class.java)
     }
+
+    single<CoroutineContext> { Dispatchers.IO }
+
+    factory<RemoteRepository> {
+        RemoteRepositoryImpl(get())
+    }
+
+    factoryOf(::GetSmartphonesUseCase)
 }
