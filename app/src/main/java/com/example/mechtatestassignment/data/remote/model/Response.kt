8cf252bfc5ad2f802f33079c1d64df9ffd6e2b75
Bekzat <altyn.bekzat01@gmail.com>
2024-05-19 16:02:47 +0500

    Response handler added

--- app/src/main/java/com/example/mechtatestassignment/data/remote/model/Response.kt
+++ app/src/main/java/com/example/mechtatestassignment/data/remote/model/Response.kt
@@ -1,8 +1,20 @@
 package com.example.mechtatestassignment.data.remote.model
 
 data class Response<T>(
-    val result:Boolean?,
+    val result: Boolean,
     val data: T?,
-    val errors:List<Any?>,
+    val errors: List<String?>,
 )
 
+
+inline fun <T> Response<T>.handle(
+    onSuccess: (Response<T>) -> Unit
+) {
+    if (isOk) return onSuccess(this)
+
+    if (!result && errors.isNotEmpty()) throw kotlin.Exception(errors[0])
+
+    if (errors.isEmpty()) throw kotlin.Exception("Неизвестный код ошибки")
+}
+
+val Response<*>.isOk: Boolean get() = data != null && result
