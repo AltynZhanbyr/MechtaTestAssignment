5478301c36cdaf6997566b3f288e6306da6c2ad6
Bekzat <altyn.bekzat01@gmail.com>
2024-05-19 10:53:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mechtatestassignment/ui/navigation/NavigationExtension.kt
@@ -0,0 +1,1 @@
+package com.example.mechtatestassignment.ui.navigation
---
+++ app/src/main/java/com/example/mechtatestassignment/ui/navigation/Screen.kt
@@ -0,0 +1,6 @@
+package com.example.mechtatestassignment.ui.navigation
+
+sealed class Screen(val route: String) {
+    data object MainScreen : Screen("mainScreen")
+    data object DetailsScreen : Screen("mainScreen/details")
+}
